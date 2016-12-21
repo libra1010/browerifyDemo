eb163541b1663384a8c1e0f3178b6fd5f62bdf78
libra1010 <libra0907@outlook.com>
2016-12-21 13:40:09 +0800

    加入新的代码

---
+++ src/Jquery.1.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by clam on 2016/12/21.
+ */
+import * as $ from "jquery" //这里导入了jquery模块，所以使用browerify 会自动将jquery打包到jquery.1.js文件里面(PS:jquery.1.js 为gulpfile文件里面配置)
+
+
+
+$(() => { console.log('hello!'); } );
+
